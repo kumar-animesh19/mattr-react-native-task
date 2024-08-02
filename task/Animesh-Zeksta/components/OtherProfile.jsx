83e0dfb4af0b33383972bb493e8e6694cfe87c6d
kumar-animesh19 <kumaranimesh1905@gmail.com>
2024-08-02 17:33:23 +0530

    "Updated babel config to include react-native-dotenv plugin, refactored OtherProfile and UserProfile components to use ScrollView for image carousel, and updated package dependencies."

--- task/Animesh-Zeksta/components/OtherProfile.jsx
+++ task/Animesh-Zeksta/components/OtherProfile.jsx
@@ -1,123 +1,179 @@
-import React, { useState } from 'react'
-import { View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
-import moment from 'moment';
-import { useRoute, useNavigation } from '@react-navigation/native';
-import SwiperFlatList from 'react-native-swiper-flatlist';
-import Icon from 'react-native-vector-icons/Ionicons';
+import React, { useState } from "react";
+import {
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+} from "react-native";
+import moment from "moment";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/Ionicons";
 
-const OtherProfile = () => {  
+const screenWidth = Dimensions.get("window").width;
+
+const OtherProfile = () => {
   const [liked, setLiked] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
   const navigation = useNavigation();
   const route = useRoute();
   const { connection } = route.params;
+
   const calculateAge = (dob) => {
-    return moment().diff(moment(dob, 'DD/MM/YYYY'), 'years');
+    return moment().diff(moment(dob, "DD/MM/YYYY"), "years");
   };
+
+  const onScroll = (event) => {
+    const contentOffsetX = event.nativeEvent.contentOffset.x;
+    const index = Math.floor(contentOffsetX / screenWidth);
+    setCurrentPage(index);
+  };
+
   return (
-    <View>
-        <View>
-            <TouchableOpacity style={styles.backButton} onPress={()=>navigation.goBack()}>
-                <Icon name="close" size={30} color="#ce1694" />
-            </TouchableOpacity>
-            <SwiperFlatList
-                autoplay
-                autoplayDelay={3}
-                autoplayLoop
-                index={0}
-                showPagination
-                paginationStyle={styles.pagination}
-                paginationActiveColor="#ce1694"
-                paginationDefaultColor="#ccc"
-            >
-            {connection.photos.map((source, index) => (
-                <View style={styles.slide} key={index}>
-                    <Image style={styles.image} source={source.path} />
-                </View>
-            ))}
-            </SwiperFlatList> 
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.backButton}
+        onPress={() => navigation.goBack()}
+      >
+        <Icon name="close" size={30} color="black" />
+      </TouchableOpacity>
+      <View style={styles.carouselContainer}>
+        <ScrollView
+          horizontal
+          pagingEnabled
+          onScroll={onScroll}
+          scrollEventThrottle={16}
+          style={styles.imageCarousel}
+        >
+          {connection.photos.map((image, index) => (
+            <Image
+              key={index}
+              source={{ uri: image.path }}
+              style={[styles.image, { width: screenWidth }]}
+            />
+          ))}
+        </ScrollView>
+        <View style={styles.pagination}>
+          {connection.photos.map((_, index) => (
+            <View
+              key={index}
+              style={[
+                styles.dot,
+                {
+                  backgroundColor: currentPage === index ? "#ce1694" : "#bbb",
+                },
+              ]}
+            />
+          ))}
         </View>
-        <View>
-            <View style={styles.details}>
-                <Text style={styles.name}>{connection.first_name} {connection.last_name}, {calculateAge(connection.dob)}
-                    <TouchableOpacity onPress={()=> setLiked(!liked)} style={styles.likeButton}>
-                        <Icon name={liked ? "heart" : "heart-outline"} size={30} color="red" />
-                    </TouchableOpacity>
-                </Text>
-                <Text style={styles.location}>{connection.location.city}, {connection.location.country}</Text>
-                <Text style={styles.description}>{ connection.bio }</Text>
-                <Text style={styles.interestText}> Interests </Text>
-                <View style={styles.interestRow}>
-                    {connection.interests.map((interest,index) => (
-                            <View style={styles.interestCol} key={index}>
-                                <Text style={styles.interest}> {interest.name} </Text>
-                            </View >
-                        ))}
-                </View>
+      </View>
+      <View style={styles.details}>
+        <View style={styles.likeButton}>
+          <Text style={styles.name}>
+            {connection.first_name} {connection.last_name},{" "}
+            {calculateAge(connection.dob)}
+          </Text>
+          <TouchableOpacity onPress={() => setLiked(!liked)}>
+            <Icon
+              name={liked ? "heart" : "heart-outline"}
+              size={30}
+              color="red"
+            />
+          </TouchableOpacity>
+        </View>
+
+        <Text style={styles.location}>
+          {connection.location.city}, {connection.location.country}
+        </Text>
+        <Text style={styles.description}>{connection.bio}</Text>
+        <Text style={styles.interestText}>Interests</Text>
+        <View style={styles.interestRow}>
+          {connection.interests.map((interest, index) => (
+            <View style={styles.interestCol} key={index}>
+              <Text style={styles.interest}>{interest.name}</Text>
             </View>
+          ))}
         </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    backButton: {
-        position: 'absolute',
-        top: 50,
-        left: 20,
-        zIndex: 1,
-    },
-    image: {
-        width: '100%',
-        height: 500,
-    },
-    slide: {
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    pagination: {
-        bottom: 10,
-    },
-    details: {
-        paddingHorizontal: 20,
-        paddingVertical: 20,
-        paddingBottom: 20
-    },
-    name: {
-        fontSize: 22,
-        fontWeight: 'bold',
-    },
-    location: {
-        fontSize: 20,
-        marginBottom: 10,
-        fontWeight: '400'
-    },
-    description: {
-        paddingVertical: 20,
-        fontSize: 15,
-    },
-    interestText: {
-        fontSize: 16,
-        fontWeight: 'bold'
-    },
-    interestRow: {
-        flexDirection: 'row',
-        marginVertical: 10
-    },
-    interestCol: {
-        paddingHorizontal:20,
-        paddingVertical:5,
-        marginHorizontal:3,
-        backgroundColor: '#ce1694',
-        borderRadius: 15,
-    },
-    interest: {
-        color: 'white',
-        textTransform: 'uppercase',
-        fontSize: 11
-    },
-    likeButton: {
-        paddingLeft: 50,
-    },
-});  
+  backButton: {
+    position: "absolute",
+    top: 50,
+    left: 20,
+    zIndex: 1,
+  },
+  carouselContainer: {
+    position: "relative",
+  },
+  imageCarousel: {
+    height: 400,
+  },
+  image: {
+    height: 400,
+    resizeMode: "cover",
+  },
+  pagination: {
+    position: "absolute",
+    bottom: 10,
+    left: 0,
+    right: 0,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  dot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    margin: 3,
+  },
+  details: {
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    paddingBottom: 20,
+  },
+  name: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  location: {
+    fontSize: 20,
+    marginBottom: 10,
+    fontWeight: "400",
+  },
+  description: {
+    paddingVertical: 20,
+    fontSize: 15,
+  },
+  interestText: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  interestRow: {
+    flexDirection: "row",
+    marginVertical: 10,
+  },
+  interestCol: {
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    marginHorizontal: 3,
+    backgroundColor: "#ce1694",
+    borderRadius: 15,
+  },
+  interest: {
+    color: "white",
+    textTransform: "uppercase",
+    fontSize: 11,
+  },
+  likeButton: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+});
 
 export default OtherProfile;
--- task/Animesh-Zeksta/components/UserProfile.jsx
+++ task/Animesh-Zeksta/components/UserProfile.jsx
@@ -1,57 +1,77 @@
-import React, { useState } from 'react'
-import { View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
-import data from '../data.json'
+import React, { useState } from 'react';
+import { View, Image, StyleSheet, Text, TouchableOpacity, ScrollView, Dimensions, Animated } from 'react-native';
+import data from '../data.json';
 import moment from 'moment';
 import { useNavigation } from '@react-navigation/native';
-import SwiperFlatList from 'react-native-swiper-flatlist';
 import Icon from 'react-native-vector-icons/Ionicons';
+
+const screenWidth = Dimensions.get('window').width;
 
 const UserProfile = () => {
     const navigation = useNavigation();
+    const [currentPage, setCurrentPage] = useState(0);
+
     const calculateAge = (dob) => {
         return moment().diff(moment(dob, 'DD/MM/YYYY'), 'years');
-      };
-  return (
-    <View>
-        <View>
-            <TouchableOpacity style={styles.backButton} onPress={()=>navigation.goBack()}>
-                <Icon name="close" size={30} color="#ce1694" />
+    };
+
+    const onScroll = (event) => {
+        const contentOffsetX = event.nativeEvent.contentOffset.x;
+        const index = Math.floor(contentOffsetX / screenWidth);
+        setCurrentPage(index);
+    };
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+                <Icon name="close" size={30} color="black" />
             </TouchableOpacity>
-            <SwiperFlatList
-                autoplay
-                autoplayDelay={3}
-                autoplayLoop
-                index={0}
-                showPagination
-                paginationStyle={styles.pagination}
-                paginationActiveColor="#ce1694"
-                paginationDefaultColor="#ccc"
-            >
-            {data[0].photos.map((source, index) => (
-                <View style={styles.slide} key={index}>
-                    <Image style={styles.image} source={source.path} />
+            <View style={styles.carouselContainer}>
+                <ScrollView
+                    horizontal
+                    pagingEnabled
+                    onScroll={onScroll}
+                    scrollEventThrottle={16}
+                    style={styles.imageCarousel}
+                >
+                    {data[0].photos.map((image, index) => (
+                        <Image
+                            key={index}
+                            source={{ uri: image.path }}
+                            style={styles.image}
+                        />
+                    ))}
+                </ScrollView>
+                <View style={styles.pagination}>
+                    {data[0].photos.map((_, index) => (
+                        <View
+                            key={index}
+                            style={[
+                                styles.dot,
+                                {
+                                    backgroundColor: currentPage === index ? '#ce1694' : '#bbb',
+                                },
+                            ]}
+                        />
+                    ))}
                 </View>
-            ))}
-            </SwiperFlatList> 
-        </View>
-        <View>
+            </View>
             <View style={styles.details}>
                 <Text style={styles.name}>{data[0].first_name} {data[0].last_name}, {calculateAge(data[0].dob)}</Text>
                 <Text style={styles.location}>{data[0].location.city}, {data[0].location.country}</Text>
-                <Text style={styles.description}>{ data[0].bio }</Text>
-                <Text style={styles.interestText}> Interests </Text>
+                <Text style={styles.description}>{data[0].bio}</Text>
+                <Text style={styles.interestText}>Interests</Text>
                 <View style={styles.interestRow}>
-                    {data[0].interests.map((interest,index) => (
+                    {data[0].interests.map((interest, index) => (
                         <View style={styles.interestCol} key={index}>
-                            <Text style={styles.interest}> {interest.name} </Text>
-                        </View >
+                            <Text style={styles.interest}>{interest.name}</Text>
+                        </View>
                     ))}
                 </View>
             </View>
         </View>
-    </View>
-  )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     backButton: {
@@ -60,16 +80,30 @@ const styles = StyleSheet.create({
         left: 20,
         zIndex: 1,
     },
-    image: {
-        width: '100%',
+    carouselContainer: {
+        position: 'relative',
+    },
+    imageCarousel: {
         height: 400,
     },
-    slide: {
-        justifyContent: 'center',
-        alignItems: 'center',
+    image: {
+        width: screenWidth,
+        height: 400,
+        resizeMode: 'cover',
     },
     pagination: {
+        position: 'absolute',
         bottom: 10,
+        left: 0,
+        right: 0,
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    dot: {
+        width: 8,
+        height: 8,
+        borderRadius: 4,
+        margin: 3,
     },
     details: {
         paddingHorizontal: 20,
@@ -98,9 +132,9 @@ const styles = StyleSheet.create({
         marginVertical: 10
     },
     interestCol: {
-        paddingHorizontal:20,
-        paddingVertical:5,
-        marginHorizontal:3,
+        paddingHorizontal: 20,
+        paddingVertical: 5,
+        marginHorizontal: 3,
         backgroundColor: '#ce1694',
         borderRadius: 15,
     },
@@ -109,6 +143,6 @@ const styles = StyleSheet.create({
         textTransform: 'uppercase',
         fontSize: 11
     },
-});  
+});
 
 export default UserProfile;
